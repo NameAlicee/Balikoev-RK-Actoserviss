cda464afe38e2d1747b2299887db1b6bc029ae15
TomaTheGreat <t.balikoev@gmail.com>
2021-12-14 15:27:34 +0300

    aa

---
+++ Autoservice/Autoservice/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Autoservice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.a0 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.a1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.a2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.a3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.a4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.a5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.a6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.a7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.a8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.a9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.a10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.a11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "10",
+            "50",
+            "100"});
+            this.comboBox1.Location = new System.Drawing.Point(651, 14);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(255, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tw Cen MT", 11.25F);
+            this.label1.Location = new System.Drawing.Point(573, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Отображать ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tw Cen MT", 11.25F);
+            this.label2.Location = new System.Drawing.Point(909, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "записей на страницу";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Возрастанию",
+            "Убыванию"});
+            this.comboBox2.Location = new System.Drawing.Point(126, 15);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(255, 21);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tw Cen MT", 11.25F);
+            this.label3.Location = new System.Drawing.Point(33, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Сортировать по";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(225)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Tw Cen MT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(156)))), ((int)(((byte)(27)))));
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Tw Cen MT", 11.25F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(156)))), ((int)(((byte)(27)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.a0,
+            this.a1,
+            this.a2,
+            this.a3,
+            this.a4,
+            this.a5,
+            this.a6,
+            this.a7,
+            this.a8,
+            this.a9,
+            this.a10,
+            this.a11});
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Tw Cen MT", 11.25F);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(156)))), ((int)(((byte)(27)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.GridColor = System.Drawing.Color.Silver;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 60);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1013, 531);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(156)))), ((int)(((byte)(26)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Tw Cen MT", 11.25F);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(782, 597);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 25);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Tw Cen MT", 11.25F);
+            this.button2.Location = new System.Drawing.Point(963, 597);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 25);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // a0
+            // 
+            this.a0.HeaderText = "№";
+            this.a0.Name = "a0";
+            // 
+            // a1
+            // 
+            this.a1.HeaderText = "Пол";
+            this.a1.Name = "a1";
+            // 
+            // a2
+            // 
+            this.a2.HeaderText = "Имя";
+            this.a2.Name = "a2";
+            // 
+            // a3
+            // 
+            this.a3.HeaderText = "Фамилия";
+            this.a3.Name = "a3";
+            // 
+            // a4
+            // 
+            this.a4.HeaderText = " Отчество";
+            this.a4.Name = "a4";
+            // 
+            // a5
+            // 
+            this.a5.HeaderText = "Дата рождения";
+            this.a5.Name = "a5";
+            // 
+            // a6
+            // 
+            this.a6.HeaderText = "Телефон";
+            this.a6.Name = "a6";
+            // 
+            // a7
+            // 
+            this.a7.HeaderText = "Email";
+            this.a7.Name = "a7";
+            // 
+            // a8
+            // 
+            this.a8.HeaderText = "Дата Добавления";
+            this.a8.Name = "a8";
+            // 
+            // a9
+            // 
+            this.a9.HeaderText = "Дата последнего посещения";
+            this.a9.Name = "a9";
+            // 
+            // a10
+            // 
+            this.a10.HeaderText = "Количество посещений";
+            this.a10.Name = "a10";
+            // 
+            // a11
+            // 
+            this.a11.HeaderText = "Теги";
+            this.a11.Name = "a11";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1037, 626);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Клиенты";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn a0;
+        private System.Windows.Forms.DataGridViewTextBoxColumn a1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn a2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn a3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn a4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn a5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn a6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn a7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn a8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn a9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn a10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn a11;
+    }
+}
+
---
+++ Autoservice/Autoservice/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Autoservice
+{
+    public partial class Form1 : Form
+    {
+        string Line, path = @"C:\Users\gabon\source\repos\Autoservice\Autoservice\Resources\clients.txt";
+        string[] array;
+        int linesCnt;
+        int CurrentRow;
+        public Form1()
+        {
+            InitializeComponent();
+            this.MinimumSize = this.Size;
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            linesCnt = File.ReadAllLines(path).Length;
+            array = new string[linesCnt];
+            filltable(comboBox1.SelectedIndex, comboBox2.SelectedIndex);
+        }
+        public void filltable(int index, int sort)
+        {
+            if (index == 0) index = 10;
+            if (index == 1) index = 50;
+            if (index == 2) index = 100;
+            dataGridView1.Rows.Clear();
+            for (int i = 0; i < index; i++)
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1[0, i].Value = i + 1;
+                dataGridView1[1, i].Value = "Пол";
+                dataGridView1[2, i].Value = "Имя";
+                dataGridView1[3, i].Value = "Фамилия";
+                dataGridView1[4, i].Value = "Отчество";
+                dataGridView1[5, i].Value = "Дата рождения";
+                dataGridView1[6, i].Value = "Телефон";
+                dataGridView1[7, i].Value = "Email";
+                dataGridView1[8, i].Value = "Дата добавления";
+                dataGridView1[9, i].Value = "Дата последнего посещения";
+                dataGridView1[10, i].Value = "Количество посещений";
+                dataGridView1[11, i].Value = "Теги";
+            }
+
+            if (sort == 0) dataGridView1.Sort(dataGridView1.Columns[0],ListSortDirection.Ascending);
+            if (sort == 1) dataGridView1.Sort(dataGridView1.Columns[0], ListSortDirection.Descending);
+            
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.White;
+            button1.ForeColor = Color.Black;
+            button2.BackColor = Color.FromArgb(255, 156, 26);
+            button2.ForeColor = Color.White;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Form2 secondform = new Form2();
+            secondform.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.BackColor = Color.White;
+            button2.ForeColor = Color.Black;
+            button1.BackColor = Color.FromArgb(255, 156, 26);
+            button1.ForeColor = Color.White;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            filltable(comboBox1.SelectedIndex, comboBox2.SelectedIndex);
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            filltable(comboBox1.SelectedIndex, comboBox2.SelectedIndex);
+        }
+    }
+}
